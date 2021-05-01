8ddbe42d02cf190e6ede1a35e713218e0f553f09
Sally <sally1eric2@gmail.com>
2021-05-01 11:21:12 +0900

    [Upload] Tab Layout

--- Front-end/app/src/main/java/edu/skku2/map/ice3037/MainActivity.java
+++ Front-end/app/src/main/java/edu/skku2/map/ice3037/MainActivity.java
@@ -1,14 +1,87 @@
 package edu.skku2.map.ice3037;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+//    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private HomeFragment homeFragment;
+    private InfoFragment infoFragment;
+    private MyPageFragment mypageFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab_layout);
+
+        homeFragment = new HomeFragment();
+        infoFragment = new InfoFragment();
+        mypageFragment = new MyPageFragment();
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
+        viewPagerAdapter.addFragment(homeFragment, "Home");
+        viewPagerAdapter.addFragment(infoFragment, "Info");
+        viewPagerAdapter.addFragment(mypageFragment, "MyPage");
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.tab_home);
+        tabLayout.getTabAt(1).setIcon(R.drawable.tab_info);
+        tabLayout.getTabAt(2).setIcon(R.drawable.tab_mypage);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments = new ArrayList<>();
+        private List<String> fragmentTitle = new ArrayList<>();
+
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior){
+            super(fm, behavior);
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            fragments.add(fragment);
+            fragmentTitle.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitle.get(position);
+        }
     }
 }
