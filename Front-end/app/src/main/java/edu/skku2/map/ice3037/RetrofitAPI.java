9d5ecaf6b388140c015b3307befbcd4b5c362874
Sally <sally1eric2@gmail.com>
2021-05-17 21:25:48 +0900

    주석 풀었습니다

--- Front-end/app/src/main/java/edu/skku2/map/ice3037/RetrofitAPI.java
+++ Front-end/app/src/main/java/edu/skku2/map/ice3037/RetrofitAPI.java
@@ -1,27 +1,27 @@
-//package edu.skku2.map.ice3037;
-//
-//import org.json.JSONArray;
-//
-//import java.util.Map;
-//
-//import retrofit2.Call;
-//import retrofit2.http.Body;
-//import retrofit2.http.Field;
-//import retrofit2.http.FieldMap;
-//import retrofit2.http.FormUrlEncoded;
-//import retrofit2.http.GET;
-//import retrofit2.http.POST;
-//
-//public interface RetrofitAPI {
-//
-//    @FormUrlEncoded
-//    @POST("/signup")
-//    Call<PostSignUp> signUp(@Field("userId") String userId,
-//                            @Field("password") String password,
-//                            @Field("creonAccount") String creonAccount);
-//
-//    @FormUrlEncoded
-//    @POST("/login")
-//    Call<PostSignUp> login(@Field("userId") String userId,
-//                           @Field("password") String password);
-//}
+package edu.skku2.map.ice3037;
+
+import org.json.JSONArray;
+
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface RetrofitAPI {
+
+    @FormUrlEncoded
+    @POST("/signup")
+    Call<PostSignUp> signUp(@Field("userId") String userId,
+                            @Field("password") String password,
+                            @Field("creonAccount") String creonAccount);
+
+    @FormUrlEncoded
+    @POST("/login")
+    Call<PostSignUp> login(@Field("userId") String userId,
+                           @Field("password") String password);
+}
