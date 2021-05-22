744693f046e46fd64b3ce402e0fe805e0f635de1
sy3136 <42471001+sy3136@users.noreply.github.com>
2021-05-23 01:32:36 +0900

    creon자동로그인, slack(부분구현)

---
+++ server/auto_login.py
@@ -0,0 +1,15 @@
+from pywinauto import application
+import time
+import os
+
+os.system('taskkill /IM coStarter* /F /T')
+os.system('taskkill /IM CpStart* /F /T')
+os.system('taskkill /IM DibServer* /F /T')
+os.system('wmic process where "name like \'%coStarter%\'" call terminate')
+os.system('wmic process where "name like \'%CpStart%\'" call terminate')
+os.system('wmic process where "name like \'%DibServer%\'" call terminate')
+time.sleep(5)
+
+app = application.Application()
+app.start('C:\CREON\STARTER\coStarter.exe /prj:cp /id:1 /pwd:11 /pwdcert:11 /autostart')
+time.sleep(60)
