6101abe3501387bd1678d92e888be15e077aa681
sy3136 <42471001+sy3136@users.noreply.github.com>
2021-05-26 21:50:43 +0900

    알림 수정

--- server/test_alarm.py
+++ server/test_alarm.py
@@ -1,5 +1,10 @@
-from creonTrade import creonTrade
 from FCM import FCM
+from pymongo import MongoClient
 
-creonTradeObj = creonTrade(FCM)
-creonTradeObj.buyOrder("A000270", 0)
+client = MongoClient(
+    "mongodb+srv://yoo:789retry@cluster0.pidsj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.Project
+users = db.userData.find()
+for user in users:
+    print("token값: ", user['token'])
+    FCM.sendMessage("매도", "{}원에 자동으로 {}을(를) 매도했습니다".format(10, "NAVER"), user['token'])
